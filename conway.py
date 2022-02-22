9cf5de674b079008726fa5232b4b6440c9816f06
Carlos Avila <angersinmeliodas@gmail.com>
2022-02-21 22:13:01 -0600

    Added life form verification and report maker

--- conway.py
+++ conway.py
@@ -7,10 +7,15 @@ import sys, argparse
 import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
+from tabulate import tabulate
+from datetime import date
+from cells import *
 
 ON = 255
 OFF = 0
 vals = [ON, OFF]
+
+existances = {'spaceship':0, 'glider':0, 'blinker':0, 'toad':0, 'beacon':0, 'block':0, 'beehive':0, 'loaf':0, 'boat':0, 'tub':0}
 
 def randomGrid(width, height):
     """returns a grid of NxN random values"""
@@ -23,7 +28,8 @@ def addGlider(i, j, grid):
                        [0,  255, 255]])
     grid[i:i+3, j:j+3] = glider
 
-def update(frameNum, img, grid, width, height):
+def update(frameNum, img, grid, width, height, generations):
+    global generation_array
     # copy grid since we require 8 neighbors for calculation
     # and we go line by line 
     newGrid = np.zeros(width*height).reshape(width, height)
@@ -42,11 +48,13 @@ def update(frameNum, img, grid, width, height):
                 if neighbours == 3 or neighbours == 2: newGrid[x, y] = ON
                 else: newGrid[x, y] = OFF
             elif neighbours == 3: newGrid[x, y] = ON
-            
 
     # update data
     img.set_data(newGrid)
     grid[:] = newGrid[:]
+    generation_array.append(newGrid.copy())
+    if len(generation_array) == generations:
+        report_lifes(width, height)
     return img, 
 
 def input_file():
@@ -61,6 +69,40 @@ def input_file():
     file.close()
     return width, height, generations, grid
 
+def report_lifes(width, height):
+    global generation_array
+    file_text = f"Simulation at {date.today()}\n"
+    file_text += f"Universe size {width} X {height}\n\n"
+    for num, gen in enumerate(generation_array):
+        print(f"Analizing ({num + 1}/{len(generation_array)})")
+        file_text += f"Iteration: {num + 1}\n"
+        for key in existances.keys():
+            existances[key] = 0
+        for x in range(len(gen)):
+            for y in range(len(gen[x])):
+                if gen[x, y] == 255:
+                    cell_type = find_life_type(x, y, gen)
+                    if cell_type:
+                        existances[cell_type] += 1
+        total_lifes = max(sum(existances.values()), 1)
+        file_text += tabulate([ ["", "Count", "Percent"],
+                                ["Block", existances['block'], round(existances['block'] / total_lifes * 100, 2)],
+                                ["Beehive", existances['beehive'], round(existances['beehive'] / total_lifes * 100, 2)],
+                                ["Loaf", existances['loaf'], round(existances['loaf'] / total_lifes * 100, 2)],
+                                ["Boat", existances['boat'], round(existances['boat'] / total_lifes * 100, 2)],
+                                ["Tub", existances['tub'], round(existances['tub'] / total_lifes * 100, 2)],
+                                ["Blinker", existances['blinker'], round(existances['blinker'] / total_lifes * 100, 2)],
+                                ["Toad", existances['toad'], round(existances['toad'] / total_lifes * 100, 2)],
+                                ["Beacon", existances['beacon'], round(existances['beacon'] / total_lifes * 100, 2)],
+                                ["Glider", existances['glider'], round(existances['glider'] / total_lifes * 100, 2)],
+                                ["LG sp ship", existances['spaceship'], round(existances['spaceship'] / total_lifes * 100, 2)],
+                                ["----------", "----------", "----------"],
+                                ["Total", total_lifes, ""]],
+                                headers="firstrow", tablefmt="psql") + "\n\n"
+    output = open("Output.txt", "w")
+    output.write(file_text[:-2])
+    print("Finished analyzing, please check output.txt.")
+
 # main() function
 def main():
     # Command line args are in sys.argv[1], sys.argv[2] ..
@@ -68,21 +110,27 @@ def main():
     # parse arguments
     parser = argparse.ArgumentParser(description="Runs Conway's Game of Life system.py.")
     # TODO: add arguments
-    read_file = True
-    if read_file:
-        width, height, generations, grid = input_file()
-    else:
-        #declare size of universe
-        width = int(input("Width of universe (default 100): "))
-        height = int(input("Height of universe (default 100): "))
+    print("1.- Use configuration on file named Input.txt")
+    print("2.- Enter values")
+    out = False
+    while not out:
+        read_file = int(input("Enter your choice: "))
+        if read_file == 1:
+            width, height, generations, grid = input_file()
+            out = True
+        elif read_file == 2:
+            #declare size of universe
+            width = int(input("Width of universe (default 100): ") or 100 ) 
+            height = int(input("Height of universe (default 100): ") or 100) 
 
-        # declare number of generations
-        generations = int(input("Number of generations (default 200): "))
-        
-        # declare grid
-        grid = np.array([])
-        # populate grid with random on/off - more off than on
-        grid = randomGrid(width, height)
+            # declare number of generations
+            generations = int(input("Number of generations (default 200): ") or 200)
+            
+            # declare grid
+            grid = np.array([])
+            # populate grid with random on/off - more off than on
+            grid = randomGrid(width, height)
+            out = True
 
     # set animation update interval
     updateInterval = 50
@@ -94,7 +142,7 @@ def main():
     # set up animation
     fig, ax = plt.subplots()
     img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, width, height, ),
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, width, height, generations, ),
                                   frames = generations,
                                   interval=updateInterval,
                                   save_count=100,
@@ -103,4 +151,5 @@ def main():
 
 # call main
 if __name__ == '__main__':
+    generation_array = []
     main()
